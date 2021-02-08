48633ecb62279151fea08d1cfe55c4f85880937f
William Castillo <willcastl3@outlook.com>
2021-02-08 00:01:45 -0600

    Homework 2

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,39 @@
-﻿//write include statements
-
-
-//write namespace using statement for cout
-
+﻿#include "variables.h"
+#include <iostream>
+#include <iomanip>
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+    double meal_amount, tip_rate, tip_amount, tax_amount, total;
 
-	return 0;
+    std::cout << "\n";
+    std::cout << "Enter Meal Total: $";
+    std::cin >> meal_amount;
+
+    tax_amount = get_sales_tax(meal_amount);
+
+    std::cout << "Enter Tip Rate:   $";
+    std::cin >> tip_rate;
+    std::cout << "\n";
+    
+    tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+    total = tip_amount + tax_amount + meal_amount;
+
+    std::cout << "Meal Amount: $";
+    std::cout << meal_amount << "\n";
+
+    std::cout << "Sales Tax: " << std::setw(3) << "$";
+    std::cout << tax_amount << "\n";
+
+    std::cout << "Tip Amount: " << std::setw(2) << "$";
+    std::cout << tip_amount << "\n";
+
+    std::cout << "Total: " << std::setw(7) << "$";
+    std::cout << total << "\n";
+
+    return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -11,4 +11,15 @@ int add_numbers(int num1, int num2)
 
 
 
+double get_sales_tax(double meal_amount)
+{	
+	const double tax_rate = 0.0675;
+
+	return tax_rate * meal_amount;
+}
+
+double get_tip_amount(double meal_amount,double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
 
