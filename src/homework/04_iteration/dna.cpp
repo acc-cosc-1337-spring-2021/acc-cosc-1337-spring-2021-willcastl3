39e2fc9affbaa4d120c68a03ea297e2f37cb052f
willcastl3 <willcastl3@outlook.com>
2021-03-26 23:36:25 -0500

    HOMEWORK 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,30 +1,60 @@
 #include "dna.h"
-/*
-Write code for function get_gc_content that accepts
-a const reference string parameter and returns a double.
-Calculate GC content:
-Iterate string count Gs and Cs, divide count by string length.
-Return quotient.
-*/
+
+double get_gc_content(const std::string& dna)
+{
+
+    int count = 0;
+    for (const char& i : dna )
+    {
+        if (i == 'C' || i == 'G')
+        {
+            count++;
+        }
+    }
+    double size_of_string;
+    size_of_string= dna.size();
+
+    return count/size_of_string;
+};
+
+
+std::string get_dna_complement(std::string dna)
+{
+    std::string complement;
+
+    for(auto letter: reverse_string(dna))
+    {
+        if(letter == 'T')
+        {
+            complement.append("A");
+        }
+        else if(letter == 'A')
+        {
+            complement.append("T");
+        }
+        else if(letter == 'C')
+        {
+            complement.append("G");
+        }
+        else if(letter == 'G')
+        {
+            complement.append("C");
+        }
+    }
+    return complement;
+};
+
+
+std::string reverse_string(std::string dna)
+{
+    std::string reversed_dna;
+    for (int i = dna.size() - 1; i >= 0; i--) {
+        reversed_dna = reversed_dna.append(1, dna[i]);
+    };
+
+    return reversed_dna;
+};
 
 
 
-
-/*
-Write code for function get_reverse_string that
-accepts a string parameter and returns a string reversed.
-*/
-
-
-
-/*
-Write prototype for function get_dna_complement that
-accepts a string dna and returns a string.
-Calculate dna complement:
-a. call function get_reverse_string(dna), save results to a local string variable
-b. iterate local string variable and
-    replace A with T, T with A, C with G and G with C
-c. return string
-
-*/
 
